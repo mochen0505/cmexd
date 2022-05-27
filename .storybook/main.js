b1e58d9aabd043ac5a053fdf81d40887b329bd8f
mochen0505 <mochen0505@hotmail.com>
2022-05-27 12:22:18 +0800

    feat: remove storybook

--- .storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "stories": ["../packages/**/stories/*.stories.@(js|svelte)"],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-svelte-csf"
-  ],
-  "framework": "@storybook/svelte"
-}
