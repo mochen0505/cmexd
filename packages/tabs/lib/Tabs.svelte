c942cfe904cad79f24ba48df5cd46cc95e7811ac
mochen0505 <mochen0505@hotmail.com>
2022-06-24 18:26:27 +0800

    fix: minor

--- packages/tabs/lib/Tabs.svelte
+++ packages/tabs/lib/Tabs.svelte
@@ -3,13 +3,13 @@
   import {writable} from 'svelte/store';
   import TabHeader from './TabHeader.svelte';
 
-  const dispatch = createEventDispatcher();
-
   let className = '';
   export {className as class};
   export let gutter = 30;
 
+  const dispatch = createEventDispatcher();
   const activeTabId = writable();
+
   setContext('tabContent', {
     gutter,
     activeTabId,
